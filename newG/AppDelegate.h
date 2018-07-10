e4a19cfa3eba6141c67034b51781911a6edea9f3
Mc <940563060@qq.com>
2018-07-10 11:59:24 +0800

    Initial Commit

---
+++ newG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newG
+//
+//  Created by Mc on 2018/7/10.
+//  Copyright © 2018年 Mc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
